582661a549f7d64111868b4ec61c0797d4ff1207
lzx <1400415006@qq.com>
2017-12-24 20:41:40 +0800

    增加ejs

--- index.js
+++ index.js
@@ -5,19 +5,41 @@ var cookieSession = require('cookie-session')
 var ejs = require('ejs')
 var multer = require('multer')
 var mysql = require('mysql')
-var adminRoute = require('./route/admin')
-var webRoute = require('./route/web')
+
 var server = express();
+
+//测试md5
 var md5Obj = require('./static/md5')
 console.log(md5Obj.md5('lzx'))
+
 if(process.env.NODE_ENV==='development'){
     console.log('i am in development...')
 }
+
+//1.获取请求数据 
+//get req.query post:
+server.use(bodyParser.urlencoded({ extended: true }));
+
+// 2. cookie和session
+
+//3. 设置模板
+server.set('view engine','html')  //输出的格式
+server.engine('html', consolidate.ejs);  //指定引擎
+server.set('views','template'); //指定模板位置
+
+//4. 路由
+var adminRoute = require('./route/admin')
+var webRoute = require('./route/web')
 server.use('/admin',adminRoute)
 server.use('/web',webRoute)
+
+//5. 静态数据
 server.use(express.static(__dirname+ '/template'))
 server.get('/',(req,res)=>{
     res.send('hello')
+})
+server.get('/lzx',(req,res)=>{
+    res.render('1.ejs',{name: 'xiang'})
 })
 
 server.listen(8989);
