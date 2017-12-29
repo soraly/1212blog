4ea50791c4bcc8a4783ff6cdc063a49eedb88758
刘志翔 <soraly@163.com>
2017-12-29 18:45:03 +0800

    add what

--- index.js
+++ index.js
@@ -40,7 +40,38 @@ var webRoute = require('./route/web')
 server.use('/admin',adminRoute)
 server.use('/web',webRoute)
 
-//5. 静态数据
+server.get('/test',(req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/json'})
+    res.end('{"name":"fenfen"}')
+})
+var js_part = `<script type="text/javascript">
+for (var a=window.location.href.split("?")[1].split("&"),r={},i=0; i<a.length; i++) {
+    var b=a[i].split("=");
+    // Escape URL string
+    if (b[0] == 'url') {
+        b[1] = decodeURIComponent(b[1]);
+    }
+    r[b[0]]=b[1];
+}
+console.log(r);
+parent.upload_callback_1(r, 119598);
+</script>`;
+server.use('/returnJS',(req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.end(js_part)
+})
+// server.use('/getToken',(req,res)=>{
+//     res.writeHead(200,{'Content-Type':'text/html'})
+//     res.end(js_part)
+// })
+server.use('/getToken',(req,res)=>{
+    var url = 'http://school.igrow.cn/api/1.1b/file/upyun/form/uploadcallback?funcid=1&image-type=JPEG&image-frames=1&image-height=326&sign=a9a3cbce4a7737158ded86af025b3b58&ext-param=igr-domain%3Dpictest.haoyuyuan.com&code=200&file_size=24004&image-width=767&url=%2F2017%2F12%2F29%2F2unoien0qth3zuvnb0ng1955eiih3jqo.jpg&time=1514512627&message=ok&mimetype=image%2Fjpeg';
+    var url2 = 'http://school.igrow.cn/api/1.1b/file/upyun/form/uploadcallback_1?image-type=JPEG&image-frames=1&image-height=326&sign=a9a3cbce4a7737158ded86af025b3b58&ext-param=igr-domain%3Dpictest.haoyuyuan.com&code=200&file_size=24004&image-width=767&url=%2F2017%2F12%2F29%2F2unoien0qth3zuvnb0ng1955eiih3jqo.jpg&time=1514512627&message=ok&mimetype=image%2Fjpeg';
+    var url3 = 'http://baby.igrow.cn/api/1.1b/file/upyun/form/uploadcallback_2?semesterid=4071&image-type=JPEG&image-frames=1&image-height=223&sign=05ab5b1cab45955ab3c97735e6e4f6b9&ext-param=igr-domain%3Dimgtest.haoyuyuan.com&code=200&file_size=14454&image-width=656&url=%2F2017%2F12%2F29%2Fh2grr3qd8jr4x2t48jl2z6tvqleqa1c1.jpg&time=1514526799&message=ok&mimetype=image%2Fjpeg'
+    res.writeHead(302,{'Content-Type':'text/html','Access-Control-Allow-Origin':'*','Location':'http://localhost:8989/returnJS'})
+    res.end('{"name":"xiang"}')
+})
+
 server.use(express.static(__dirname+ '/template'))
 
 server.listen(8989);
--- route/admin/index.js
+++ route/admin/index.js
@@ -1,39 +1,72 @@
-var express = require('express')
-var router = express.Router()
-var common = require('../../common/md5')
+const express = require('express')
+const router = express.Router()
+const common = require('../../common/md5')
+const sass = require('node-sass');
+const fs = require('fs')
+//mysql的连接pool
+const pool = require('../../models/index.js');
+
+
+function complieSass(inputUrl,outPut) {
+    return new Promise((resolve, reject) => {
+        sass.render({
+            file: inputUrl,
+            outputStyle: 'expanded'
+        }, function (err, data) {
+            if (err) {
+                console.log(err, 'rrr')
+            } else {
+                var css = data.css.toString();
+
+                fs.writeFile(outPut, css, (err) => {
+                    console.log('write done')
+                    resolve()
+                })
+            }
+        })
+    })
+}
+
+
 //检查是否有session，没有就去登录
-router.get('/login',(req,res,next)=>{
-    if(req.session.user){
-        res.render('./admin/success.ejs')
-    }else {
-        res.render('./admin/login.ejs')
+router.use((req, res, next) => {
+    console.log(req.session.user_id, 'req.session.user_id')
+    if (!req.session.user_id && req.url != '/login') {
+        res.redirect('/admin/login');
+    } else {
+        next();
     }
 })
-//mysql的连接pool
-var pool = require('../../models/index.js');
-router.post('/login',(req,res)=>{
+router.get('/login', (req, res) => {
+    res.render('./admin/login.ejs')
+})
+
+router.post('/login', (req, res) => {
     var sql = `select * from user_tables where username='${req.body.username}'`;
-    pool.query(sql, (err,data)=>{
-        if(err){
+    pool.query(sql, (err, data) => {
+        if (err) {
             res.status(500).end('database err')
-        }else {
-            console.log(data,'data')
-            if(data.length){
-                if( data[0].password == common.md5(req.body.password) ){
-                    res.send('login success')
-                }else {
-                    res.send('密码错误')
+        } else {
+            console.log(data, 'data')
+            if (data.length) {
+                if (data[0].password == common.md5(req.body.password)) {
+                    req.session.user_id = data[0].id;
+                    res.redirect('/admin/');
+                } else {
+                    res.status(400).send('密码错误').end()
                 }
-            }else {
-                res.send('用户名错误')
+            } else {
+                res.status(400).send('用户名错误').end()
             }
         }
     })
 })
-router.get('/teacher1',(req,res)=>{
-    res.send('i am teacher1')
+router.get('/', (req, res) => {
+    complieSass('./assets/css/main.scss', './template/css/mainn.css').then(()=>{
+        res.render('./admin/success.ejs');
+    })    
 })
-router.get('/teacher2',(req,res)=>{
+router.get('/teacher2', (req, res) => {
     res.send('i am teacher2')
 })
 module.exports = router
